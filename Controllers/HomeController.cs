029773a845107c8b7f2acef7d5fe1841db731e52
Davidmr2 <cuenta2mryt@gmail.com>
2025-05-31 00:05:08 -0400

    segundo commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto2unidad1.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
+
+
+public class HomeController : Controller
+{
+	public ActionResult Index()
+	{
+		return View();
+	}
+
+	[HttpPost]
+	public ActionResult Saludo(string nombre)
+	{
+		ViewBag.Mensaje = "Hola " + nombre;
+		return View("Index");
+	}
+}
